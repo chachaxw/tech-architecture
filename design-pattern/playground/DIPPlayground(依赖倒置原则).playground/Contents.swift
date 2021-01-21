651f01197224e0697f8eedeae00a2977ce2411be
chacha <867571123@qq.com>
2021-01-21 12:15:48 +0800

    feat: 修改观察者模式和发布订阅模式文件命名，添加依赖倒置原则Playground

---
+++ design-pattern/playground/DIPPlayground(依赖倒置原则).playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//:
+//: ### 什么是依赖倒置原则(Dependence Inversion Principle)？
+//:
+//: 依赖倒置原则的原始定义:
+//:
+//: 高层模块不应该依赖于低层模块，两者都应该依赖于抽象。抽象不应该依赖于细节，细节应该依赖于抽象。
+//:
+//: High level modules should not depend upon low level modules.Both should depend upon abstractions.
+//: Abstractions should not depend upon details.Details should depend upon abstractions.
+//:
+
+var str = "Hello, Dependence Inversion Principle playground"
