7e95bd96ab096f6faecf224b30324b9be5e0ee94
chacha <867571123@qq.com>
2021-03-19 14:40:49 +0800

    feat: 添加工厂方法模式定义和代码内容

--- design-pattern/playground/FactoryMethodPlayground(工厂方法模式).playground/Contents.swift
+++ design-pattern/playground/FactoryMethodPlayground(工厂方法模式).playground/Contents.swift
@@ -3,7 +3,7 @@ var str = "Hello, Factory Method Pattern playground"
 
 //: ## 工厂方法模式的定义
 //:
-//: 定义一个用于创建对象的 接口，让子类决定实例化哪一个类。工厂方法使一个类的实例化延迟到其子类。
+//: 定义一个用于创建对象的接口，让子类决定实例化哪一个类。工厂方法使一个类的实例化延迟到其子类。
 //:
 //: Define an interface for creating an object,but let subclasses decide which class to instantiate.Factory Method lets a class defer instantiation to subclasses.
 //:
