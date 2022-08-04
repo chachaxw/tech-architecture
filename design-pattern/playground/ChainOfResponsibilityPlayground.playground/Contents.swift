e5091d8d21fa39e12abf385fb691f5a6ffa87219
chachaxw <867571123@qq.com>
2022-08-05 00:08:17 +0800

    feat: 添加责任链模式测试代码

--- design-pattern/playground/ChainOfResponsibilityPlayground.playground/Contents.swift
+++ design-pattern/playground/ChainOfResponsibilityPlayground.playground/Contents.swift
@@ -59,3 +59,148 @@ var greeting = "Hello, Chain of Responsibility playground"
 //: 1. 部分请求可能未被处理。
 //:
 //:
+
+import XCTest
+
+/// The handler protocol declares a method for building the chain of handlers
+/// It also declares a method for executing a request.
+protocol Handler: class {
+    
+    @discardableResult
+    func setNext(handler: Handler) -> Handler
+    
+    func handle(request: String) -> String?
+    
+    var nextHandler: Handler? { get set }
+}
+
+
+extension Handler {
+    func setNext(handler: Handler) -> Handler {
+        self.nextHandler = handler
+        
+        /// Returning a handler from here will let us link handlers in a convenient way like this:
+        /// monkey.setNext(handler: squirrel).setNext(handler: dog)
+        return handler
+    }
+    
+    func handle(request: String) -> String? {
+        return nextHandler?.handle(request: request)
+    }
+}
+
+/// All Concrete Handlers either handle a request or pass it to the next handler in the chain.
+class MonkeyHandler: Handler {
+    
+    var nextHandler: Handler?
+    
+    func handle(request: String) -> String? {
+        if (request == "Banana") {
+            return "Monkey: I'll set the " + request + ".\n"
+        } else {
+            return nextHandler?.handle(request: request)
+        }
+    }
+}
+
+class SquirrelHandler: Handler {
+
+    var nextHandler: Handler?
+
+    func handle(request: String) -> String? {
+
+        if (request == "Nut") {
+            return "Squirrel: I'll eat the " + request + ".\n"
+        } else {
+            return nextHandler?.handle(request: request)
+        }
+    }
+}
+
+class DogHandler: Handler {
+
+    var nextHandler: Handler?
+
+    func handle(request: String) -> String? {
+        if (request == "MeatBall") {
+            return "Dog: I'll eat the " + request + ".\n"
+        } else {
+            return nextHandler?.handle(request: request)
+        }
+    }
+}
+
+/// The client code is usually suited to work with a single handler. In most cases, it is not even aware that the handler is part of a chain.
+class Client {
+    static func someClientCode(handler: Handler) {
+        let food = ["Nut", "Banana", "Cup of coffee"]
+        
+        for item in food {
+            
+            print("Client: Who wants to a " + item + "?\n")
+            
+            guard let result = handler.handle(request: item) else {
+                print(" " + item + " was left untouched.\n")
+                return
+            }
+            
+            print(" " + result)
+        }
+    }
+}
+
+/// Let's see how it all works together.
+class ChainOfResponsibilityConceptual: XCTestCase {
+ 
+    func test() {
+
+        /// The other part of the client code constructs the actual chain.
+
+        let monkey = MonkeyHandler()
+        let squirrel = SquirrelHandler()
+        let dog = DogHandler()
+        monkey.setNext(handler: squirrel).setNext(handler: dog)
+
+        /// The client should be able to send a request to any handler, not just
+        /// the first one in the chain.
+
+        print("Chain: Monkey > Squirrel > Dog\n\n")
+        Client.someClientCode(handler: monkey)
+        print()
+        print("Subchain: Squirrel > Dog\n\n")
+        Client.someClientCode(handler: squirrel)
+    }
+}
+
+let test = ChainOfResponsibilityConceptual()
+
+test.test()
+
+// Output: 
+//
+// Chain: Monkey > Squirrel > Dog
+//
+//
+// Client: Who wants to a Nut?
+//
+// Squirrel: I'll eat the Nut.
+//
+// Client: Who wants to a Banana?
+//
+// Monkey: I'll set the Banana.
+//
+// Client: Who wants to a Cup of coffee?
+//
+// Cup of coffee was left untouched.
+//
+//
+// Subchain: Squirrel > Dog
+//
+//
+// Client: Who wants to a Nut?
+//
+// Squirrel: I'll eat the Nut.
+//
+// Client: Who wants to a Banana?
+//
+// Banana was left untouched.
