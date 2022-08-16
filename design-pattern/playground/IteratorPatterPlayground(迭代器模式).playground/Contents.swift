4122bb689a1a2028cd368bd199c128f06decd969
chachaxw <867571123@qq.com>
2022-08-16 22:45:51 +0800

    feat: add iterator pattern code example

--- design-pattern/playground/IteratorPatterPlayground(迭代器模式).playground/Contents.swift
+++ design-pattern/playground/IteratorPatterPlayground(迭代器模式).playground/Contents.swift
@@ -55,3 +55,120 @@ var greeting = "Hello, Iterator Pattern playground"
 //: 1. 如果你的程序只与简单的集合进行交互，应用该模式可能会矫枉过正。
 //: 2. 对于某些特殊集合，使用迭代器可能比直接遍历的效率低。
 //:
+
+import XCTest
+
+/// This is a collection that we're going to iterate through using an iterator
+/// derived from IteratorProtocol.
+class WordsCollection {
+
+    fileprivate lazy var items = [String]()
+
+    func append(_ item: String) {
+        self.items.append(item)
+    }
+}
+
+extension WordsCollection: Sequence {
+
+    func makeIterator() -> WordsIterator {
+        return WordsIterator(self)
+    }
+}
+
+/// Concrete Iterators implement various traversal algorithms. These classes
+/// store the current traversal position at all times.
+class WordsIterator: IteratorProtocol {
+
+    private let collection: WordsCollection
+    private var index = 0
+
+    init(_ collection: WordsCollection) {
+        self.collection = collection
+    }
+
+    func next() -> String? {
+        defer { index += 1 }
+        return index < collection.items.count ? collection.items[index] : nil
+    }
+}
+
+
+/// This is another collection that we'll provide AnyIterator for traversing its
+/// items.
+class NumbersCollection {
+
+    fileprivate lazy var items = [Int]()
+
+    func append(_ item: Int) {
+        self.items.append(item)
+    }
+}
+
+extension NumbersCollection: Sequence {
+
+    func makeIterator() -> AnyIterator<Int> {
+        var index = self.items.count - 1
+
+        return AnyIterator {
+            defer { index -= 1 }
+            return index >= 0 ? self.items[index] : nil
+        }
+    }
+}
+
+/// Client does not know the internal representation of a given sequence.
+class Client {
+    // ...
+    static func clientCode<S: Sequence>(sequence: S) {
+        for item in sequence {
+            print(item)
+        }
+    }
+    // ...
+}
+
+/// Let's see how it all works together.
+class IteratorConceptual: XCTestCase {
+
+    func testIteratorProtocol() {
+
+        let words = WordsCollection()
+        words.append("First")
+        words.append("Second")
+        words.append("Third")
+
+        print("Straight traversal using IteratorProtocol:")
+        Client.clientCode(sequence: words)
+    }
+
+    func testAnyIterator() {
+
+        let numbers = NumbersCollection()
+        numbers.append(1)
+        numbers.append(2)
+        numbers.append(3)
+
+        print("\nReverse traversal using AnyIterator:")
+        Client.clientCode(sequence: numbers)
+    }
+}
+
+let iterator = IteratorConceptual()
+
+iterator.testIteratorProtocol()
+
+iterator.testAnyIterator()
+
+// Output
+//
+// Straight traversal using IteratorProtocol:
+// First
+// Second
+// Third
+//
+// Reverse traversal using AnyIterator:
+// 3
+// 2
+// 1
+//
